b83d182061c99c07d47682c199859ea7a1753ce2
Rob Hoes <rob.hoes@citrix.com>
2014-05-30 13:56:09 +0100

    Update .travis-ci.sh

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -52,7 +52,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   git config --global user.name "Travis"
 
   #using token clone gh-pages branch
-  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/robhoes/elo-ladder-games.git  gh-pages > /dev/null
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/robhoes/elo-ladder-chess.git  gh-pages > /dev/null
 
   #go into directory and copy data we're interested in to that directory
   cd gh-pages
